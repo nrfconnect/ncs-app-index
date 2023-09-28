e72bae9e2ea62e22551a1039d206704a0b00a2fb
Dee Roberts <dee@deeroberts.no>
2023-09-28 17:18:22 +0200

    Validate index files (#4)
    * Validate index files
    
    * Move page deploy to workflows folder
    
    * Fix folder name
    
    * Appy suggestions
    
    * Call from pages-deploy
    
    * Rename file

---
+++ scripts/validate-index.ts
@@ -0,0 +1,47 @@
+/* Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import Ajv, { ErrorObject } from 'ajv';
+import colours from 'ansi-colors';
+
+import orgIndexSchema from '../resources/org_index_schema.json';
+import { readOrgIndexFiles } from './helpers';
+
+function reportError(file: string, error: ErrorObject) {
+    const start = `Property ${error.instancePath}`.trim();
+    const message = `${start} ${error.message}`;
+
+    if ('GITHUB_RUN_ID' in process.env) {
+        console.log(`::error file={${file}},title={Invalid index file}::${message}`);
+    } else {
+        console.error(colours.red(`Error in ${file}: ${message}`));
+    }
+}
+
+async function run() {
+    const ajv = new Ajv();
+    const files = await readOrgIndexFiles();
+
+    const validate = ajv.compile(orgIndexSchema);
+
+    let isError = false;
+
+    for (const { orgIndex, file } of files) {
+        const isValid = validate(orgIndex);
+        if (!isValid) {
+            validate.errors?.forEach((error) => reportError(file, error));
+            isError = true;
+        }
+    }
+
+    if (!isError) {
+        console.log(colours.green('All index files validated successfully.'));
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
+}
+
+run();
